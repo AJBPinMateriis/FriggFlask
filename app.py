6834ae3a42a23e71518109ac3a0fc74b52d19ff6
VidalOlmedo <vidalolmedo@outlook.com>
2019-02-25 13:38:57 -0600

    Initial file uploading works

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request
 import sys
+import os
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/home/pi/Desktop/Flask_Git/FriggFlask/uploaded_files'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -9,13 +14,21 @@ def index():
 	#variable = request.form
 	#return(sys.version) SHOWS PYTHON VERSION
 	
+	
+	
 @app.route('/getData', methods = ['POST','GET'])#if 'methods' is not declared, the default is GET
 def get_event():
 	print("Initialize printer")
-	
-	#result = request.form['texto']
-	result = request.args.get('texto')
+
+	#FORM DATA REQUEST
+	result = request.form
 	print(result)
+	print(result["texto"])
+	
+	#FILE UPLOADING
+	f = request.files['file']
+	filename = secure_filename(f.filename)
+	f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	
 	#return render_template('index.html') !DONT RENDER THE TEMPLATE, CALL THE FUNCTION WHICH DOES
 	return redirect(url_for('index')) #import 'redirect' and 'url_for' (top)
@@ -24,6 +37,8 @@ def get_event():
 def stop_event():
 	print("Stop printer")
 	return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
 	app.run(debug = True, host = '0.0.0.0')
